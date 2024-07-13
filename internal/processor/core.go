bdb4606c55c71b1e1ae15e625c5e6d7d53dae3e9
wizenheimer <nayan.kumar@couchbase.com>
2024-07-13 20:24:35 +0530

    feat: add core execution loop

--- internal/processor/core.go
+++ internal/processor/core.go
@@ -5,14 +5,16 @@ import (
 	"encoding/json"
 	"fmt"
 	"net/http"
+	"time"
 
 	"github.com/labstack/echo/v4"
 	log "github.com/wizenheimer/cascade/internal/logger"
 	"github.com/wizenheimer/cascade/service/k8x"
+	"go.uber.org/zap"
 )
 
 // Stream the response via Server Sent Event
-func Process(c echo.Context, callback kubernetesCallback) error {
+func Process(c echo.Context) error {
 	// Set Headers
 	c.Response().Header().Set(echo.HeaderContentType, "text/event-stream")
 	c.Response().Header().Set("Cache-Control", "no-cache")
@@ -32,9 +34,14 @@ func Process(c echo.Context, callback kubernetesCallback) error {
 	logger := loggerWithChan.Logger
 	logChan := loggerWithChan.LogChan
 
-	requestID := c.Request().Header.Get("X-Request-ID")
-	if requestID == "" {
-		requestID = c.Response().Header().Get(echo.HeaderXRequestID)
+	sessionID := c.Request().Header.Get("X-Request-ID")
+	if sessionID == "" {
+		sessionID = c.Response().Header().Get(echo.HeaderXRequestID)
+	}
+
+	scenario := c.Param("scenario")
+	if scenario == "" {
+		scenario = "undefined"
 	}
 
 	// Create a new context that will be cancelled when the request is done
@@ -42,7 +49,7 @@ func Process(c echo.Context, callback kubernetesCallback) error {
 	defer cancel()
 
 	// Parse Configs
-	_, _, _, _, err := k8x.ParseConfigs(c)
+	cc, tc, rc, _, err := k8x.ParseConfigs(c)
 	if err != nil {
 		// Parse the log
 		data, err := log.ParseLog("error", err.Error())
@@ -60,8 +67,49 @@ func Process(c echo.Context, callback kubernetesCallback) error {
 		return nil
 	}
 
+	// Create executor
+	executor, err := k8x.CreateExecutor(cc, tc, rc)
+	if err != nil {
+		// Parse the log
+		data, err := log.ParseLog("error", err.Error())
+		if err != nil {
+			return err
+		}
+
+		// Send response back to client
+		_, err = c.Response().Write(data)
+		if err != nil {
+			return err
+		}
+
+		c.Response().Flush()
+		return nil
+	}
+
+	// Create ticker
+	ticker := time.NewTicker(rc.Interval)
+	defer ticker.Stop()
+
 	// Start processing in a goroutine
-	go callback(ctx, logger, requestID)
+	go func(executor *k8x.Executor, sessionID string, ctx context.Context, next <-chan time.Time) {
+		for {
+			logger.Info("Chaos Session Triggered", zap.Any("Session", sessionID))
+			logger.Info("Chaos Scenario", zap.Any("Scenario", scenario))
+
+			// Trigger Execution
+			if err = executor.Execute(ctx, logger); err != nil {
+				logger.Error(err.Error())
+			}
+
+			select {
+			case <-next:
+				// trigger next session
+			case <-ctx.Done():
+				// skip subsequent execution
+				return
+			}
+		}
+	}(executor, sessionID, ctx, ticker.C)
 
 	// Stream logs back to the client
 	for {
