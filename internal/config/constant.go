9ce5d37a32fcd2422f1b4849fb3eb6eda606c5d9
wizenheimer <nayan.kumar@couchbase.com>
2024-07-13 19:39:39 +0530

    feat: add default configs

--- internal/config/constant.go
+++ internal/config/constant.go
@@ -1,3 +1,21 @@
 package config
 
-const SERVER_PORT = ":8080"
+// API Server Defaults
+const (
+	SERVER_PORT = ":8080"
+)
+
+// Executor Defaults
+const (
+	HEALTH_CHECK_PORT = ":8080"
+
+	RUNTIME_INTERVAL = "10m"
+
+	RATIO = "0.5"
+
+	MODE = "delete"
+
+	SYNC = "true"
+
+	WATCH = "true"
+)
